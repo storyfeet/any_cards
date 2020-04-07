d073ebdc83a565ab3f8e340770d80ad680999135
storyfeet <thebeaky@psmail.net>
2020-04-07 18:15:59 +0100

    now loads cards, but need to get template loaded, and written a simple template to

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+use card_format::Card as FCard;
 use clap_conf::prelude::*;
+use handlebars::Handlebars;
 
-fn main() -> Result<(), ConfError> {
+pub struct GCard<'a> {
+    tplate: &'a Handlebars<'a>,
+    card: FCard,
+}
+
+fn main() -> Result<(), failure::Error> {
     let clp = clap_app!(any_cards =>
         (about:"Makes any kind of svg card using handlebars templates")
         (version:crate_version!())
@@ -13,8 +20,20 @@ fn main() -> Result<(), ConfError> {
 
     let cfg = with_toml_env(&clp, &["any_conf.toml"]);
 
-    for v in cfg.grab_multi().arg("files").req()? {
-        println!("File = {:?}", v)
+    let tplate = Handlebars::new();
+
+    //TODO Read template in
+
+    let mut all_cards = Vec::new();
+    for fname in cfg.grab_multi().arg("files").req()? {
+        let mut f = std::fs::File::open(fname)?;
+        let loaded_cards = card_format::load_cards(&mut f)?;
+        for c in loaded_cards {
+            all_cards.push(GCard {
+                tplate: &tplate,
+                card: c,
+            });
+        }
     }
 
     Ok(())
